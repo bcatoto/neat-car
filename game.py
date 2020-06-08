8b1522a6eae7ea019516a5ffb7ec82ea97d2cd57
bcatoto <biancacatoto999@gmail.com>
2020-06-09 00:15:37 +0800

    fixed rotation

--- game.py
+++ game.py
@@ -9,7 +9,7 @@ WIN_WIDTH = 800
 WIN_HEIGHT = 600
 
 WIN = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
-pygame.display.set_caption("NEA Racing")
+pygame.display.set_caption("NEAT Racing")
 STAT_FONT = pygame.font.SysFont("arial", 50)
 
 CAR_IMG = pygame.image.load(os.path.join("imgs", "car.png")).convert_alpha()
@@ -23,32 +23,34 @@ class Car:
     Car class representing car player
     """
     VEL = 5
-
+    INITIAL_DIR = (0, 1)
+    IMG = pygame.transform.scale(CAR_IMG, (20, 44))
     TRACK_MASK = pygame.mask.from_surface(TRACK_IMG)
 
     def __init__(self, x, y, angle):
         self.x = x
         self.y = y
         self.angle = angle
-        self.dir = (0, 1)
-        self.img = pygame.transform.scale(CAR_IMG, (20, 44))
+        self.dir = self.INITIAL_DIR
+        self.img = self.IMG
         self.update_angle()
         self.update_mask()
 
     def update_angle(self):
-        x = self.dir[0]
-        y = self.dir[1]
+        x = self.INITIAL_DIR[0]
+        y = self.INITIAL_DIR[1]
         sin = math.sin(math.radians(self.angle))
         cos = math.cos(math.radians(self.angle))
         self.dir = (cos * x - sin * y, sin * x + cos * y)
-        self.img = pygame.transform.rotate(self.img, self.angle)
+        self.img = pygame.transform.rotate(self.IMG, self.angle)
+        print("angle: ", self.angle)
 
     def update_mask(self):
         self.mask = pygame.mask.from_surface(self.img)
 
     def rotate(self, angle):
         self.angle += angle
-        update_angle()
+        self.update_angle()
 
     def move(self):
         self.x += round(self.dir[0] * self.VEL)
@@ -57,10 +59,10 @@ class Car:
 
     def collide(self):
         if self.TRACK_MASK.overlap(self.mask, (self.x, self.y)):
-            print("DEAD")
+            x = 1
 
     def draw(self, win):
-        rect = self.img.get_rect(center = self.img.get_rect(topleft = (self.x, self.y)).center)
+        rect = self.img.get_rect(center = self.IMG.get_rect(topleft = (self.x, self.y)).center)
         win.blit(self.img, rect.topleft)
 
 def draw_window(win, car):
@@ -75,7 +77,7 @@ def draw_window(win, car):
 
 def eval_genomes():
     car = Car(350, 85, -90)
-
+    count = 0
     run = True
     while run:
         for event in pygame.event.get():
@@ -85,8 +87,10 @@ def eval_genomes():
                 quit()
                 break
 
+        car.rotate(-5)
         car.move()
         draw_window(WIN, car)
+        count += 1
 
 if __name__ == "__main__":
     eval_genomes()
